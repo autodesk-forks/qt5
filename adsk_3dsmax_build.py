80e3fa8825c876e071a73872a0737a7cafd4b9bd
Ulrich Haberl <Ulrich.Haberl@autodesk.com>
2016-12-14 13:21:48 +0100

    MAXX-34316 Qt5 rollup fixes et al (#3)
    Updated the build-script to build a debug or a release version of the libraries.
    Updated the qtimageformats to use the libtiff 4.0.7
    
    Used to build the 3dsmax artifact Qt-5.6.2-3dsmax-004.7z

--- adsk_3dsmax_build.py
+++ adsk_3dsmax_build.py
@@ -3,6 +3,8 @@ import os
 import shutil
 import time
 import subprocess
+import sys
+import getopt
 
 OPENSSL_DIR = '../openssl/1.0.2j'
 ICU_DIR = '../icu'
@@ -11,6 +13,33 @@ TARGET_DIR = '../qt5_dist'
 
 BUILD_DIR_DEBUG = '../qt5_build_debug'
 BUILD_DIR_RELEASE = '../qt5_build_release'
+BUILD_DIR = '../qt5_build'
+
+# ----------------------------------------------------------------------------
+try:
+    opts, args = getopt.getopt( sys.argv[1:],"dr",["debug","release"])
+except getopt.GetoptError:
+    print 'adsk_3dsmax_build.py [--debug|--release]'
+    sys.exit(2)
+
+MODE = ''
+for opt, arg in opts:
+    if opt == '-h':
+        print 'adsk_3dsmax_build.py [--debug|--release]'
+        sys.exit()
+    elif opt in ("-d", "--debug"):
+        if MODE == 'release':
+            MODE = 'debug_and_release'
+        else:
+            MODE = 'debug'
+    elif opt in ("-r", "--release"):
+        if MODE == 'debug':
+            MODE = 'debug_and_release'
+        else:
+            MODE = 'release'
+# ----------------------------------------------------------------------------
+if ( MODE == ''): 
+    MODE = 'debug_and_release'
 
 # ----------------------------------------------------------------------------
 # here we go!
@@ -42,47 +71,95 @@ ICU_LIB_PATH = os.path.realpath(
 BUILD_ENV = os.environ.copy()
 BUILD_ENV['QMAKE_CXXFLAGS'] = '-DWIN_VER=0x0601 -D_WIN32_WINNT=0x0601'
 
-if not os.path.exists(BUILD_PATH_DEBUG):
-    os.mkdir(BUILD_PATH_DEBUG)
-os.chdir(BUILD_PATH_DEBUG)
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+if MODE == 'debug' or MODE == 'debug_and_release':
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
+    if not os.path.exists(BUILD_PATH_DEBUG):
+        os.mkdir(BUILD_PATH_DEBUG)
+    os.chdir(BUILD_PATH_DEBUG)
+
+    CONFIGURE = os.path.relpath(os.path.join(SCRIPT_PATH, 'configure'))
+
+    # -------------------------------------------------------------------------
+    # copy our fancy multi-thread build tool into our build_paths
+    # -------------------------------------------------------------------------
+    shutil.copy(os.path.join(SCRIPT_PATH, 'jom.exe'),
+                os.path.join(BUILD_PATH_DEBUG, 'jom.exe'))
+
+    subprocess.check_call(
+        CONFIGURE +
+        ' -opensource -confirm-license '
+        ' -nomake examples -no-compile-examples -no-cetest '
+        ' -qt-zlib -qt-libpng '
+        ' -opengl dynamic '
+        ' -mp -prefix \"' + PREFIX_PATH + '\" '
+        ' -openssl '
+        ' OPENSSL_LIBS_DEBUG="-llibeay32 -lssleay32" ' +
+        ' OPENSSL_LIBS_RELEASE="-llibeay32 -lssleay32" ' +
+        ' -I \"' + OPENSSL_INCLUDE_PATH + '\" -L \"' + OPENSSL_LIB_PATH_DEBUG + '\"' +
+        ' -no-icu ' +  # we just enable icu for the webkit module that we compile
+                    # in a separate step
+        # ' -icu ' +
+        # ' -I \"' + ICU_INCLUDE_PATH + '\" -L \"' + ICU_LIB_PATH + '\"'
+        #' -make tests'
+        ' -debug ', env=BUILD_ENV, shell=True)
+
+    subprocess.check_call('jom', env=BUILD_ENV, shell=True)
+    subprocess.check_call('jom install', env=BUILD_ENV, shell=True)
+
+    # -------------------------------------------------------------------------
+    # we have to rename that file to keep it from getting overridden by the 
+    # build if following...
+    # -------------------------------------------------------------------------
+    if os.path.exists(os.path.join(PREFIX_PATH, 'bin', 'QtWebEngineProcessd.exe')):
+        os.remove(os.path.join(PREFIX_PATH, 'bin', 'QtWebEngineProcessd.exe'))
+
+    os.rename(os.path.join(PREFIX_PATH, 'bin', 'QtWebEngineProcess.exe'),
+            os.path.join(PREFIX_PATH, 'bin', 'QtWebEngineProcessd.exe'))
 
 # -----------------------------------------------------------------------------
-# copy our fancy multi-thread build tool into our build_paths
 # -----------------------------------------------------------------------------
-shutil.copy(os.path.join(SCRIPT_PATH, 'jom.exe'),
-            os.path.join(BUILD_PATH_DEBUG, 'jom.exe'))
+if MODE == 'release' or MODE == 'debug_and_release':
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
-CONFIGURE = os.path.relpath(os.path.join(SCRIPT_PATH, 'configure'))
+    if not os.path.exists(BUILD_PATH_RELEASE):
+        os.mkdir(BUILD_PATH_RELEASE)
+    os.chdir(BUILD_PATH_RELEASE)
 
-subprocess.check_call(
-    CONFIGURE +
-    ' -opensource -confirm-license '
-    ' -nomake examples -no-compile-examples -no-cetest '
-    ' -qt-zlib -qt-libpng '
-    ' -opengl dynamic '
-    ' -mp -prefix \"' + PREFIX_PATH + '\" '
-    ' -openssl '
-    ' OPENSSL_LIBS_DEBUG="-llibeay32 -lssleay32" ' +
-    ' OPENSSL_LIBS_RELEASE="-llibeay32 -lssleay32" ' +
-    ' -I \"' + OPENSSL_INCLUDE_PATH + '\" -L \"' + OPENSSL_LIB_PATH_DEBUG + '\"' +
-    ' -no-icu ' +  # we just enable icu for the webkit module that we compile
-                   # in a separate step
-    # ' -icu ' +
-    ' -I \"' + ICU_INCLUDE_PATH + '\" -L \"' + ICU_LIB_PATH + '\"'
-    ' -debug ', env=BUILD_ENV, shell=True)
+    CONFIGURE = os.path.relpath(os.path.join(SCRIPT_PATH, 'configure'))
+    
+    # -----------------------------------------------------------------------------
+    # copy our fancy multi-thread build tool into our build_paths
+    # -----------------------------------------------------------------------------
+    shutil.copy(os.path.join(SCRIPT_PATH, 'jom.exe'),
+                os.path.join(BUILD_PATH_RELEASE, 'jom.exe'))
 
-subprocess.check_call('jom', env=BUILD_ENV, shell=True)
-subprocess.check_call('jom install', env=BUILD_ENV, shell=True)
+    subprocess.check_call(
+        CONFIGURE +
+        ' -opensource -confirm-license '
+        ' -nomake examples -no-compile-examples -no-cetest '
+        ' -qt-zlib -qt-libpng '
+        ' -opengl dynamic '
+        ' -mp -prefix \"' + PREFIX_PATH + '\" '
+        ' -openssl OPENSSL_LIBS_DEBUG="-llibeay32 -lssleay32" '
+        ' OPENSSL_LIBS_RELEASE="-llibeay32 -lssleay32" '
+        ' -I \"' + OPENSSL_INCLUDE_PATH + '\" -L \"' + OPENSSL_LIB_PATH + '\"'
+        ' -no-icu ' # we just enable icu for the webkit module that we compile
+                    # in a separate step
+        # ' -icu ' +
+        # ' -I \"' + ICU_INCLUDE_PATH + '\" -L \"' + ICU_LIB_PATH + '\"'
+        ' -make tests'
+        ' -release ', env=BUILD_ENV, shell=True)
+
+    subprocess.check_call('jom', env=BUILD_ENV, shell=True)
+    subprocess.check_call('jom install', env=BUILD_ENV, shell=True)
 
 # -----------------------------------------------------------------------------
-# we have to rename that file to keep it from getting overridden by the release
-# build following...
 # -----------------------------------------------------------------------------
-if os.path.exists(os.path.join(PREFIX_PATH, 'bin', 'QtWebEngineProcessd.exe')):
-    os.remove(os.path.join(PREFIX_PATH, 'bin', 'QtWebEngineProcessd.exe'))
-
-os.rename(os.path.join(PREFIX_PATH, 'bin', 'QtWebEngineProcess.exe'),
-          os.path.join(PREFIX_PATH, 'bin', 'QtWebEngineProcessd.exe'))
 
 # -----------------------------------------------------------------------------
 # cleaning up unnecessary release pdbs...
@@ -95,41 +172,6 @@ for f in os.listdir(os.path.join(PREFIX_PATH, 'bin')):
 
 
 # -----------------------------------------------------------------------------
-# -----------------------------------------------------------------------------
-# DEBUG BUILD DONE - STARTING RELEASE BUILD !!!
-# -----------------------------------------------------------------------------
-# -----------------------------------------------------------------------------
-
-if not os.path.exists(BUILD_PATH_RELEASE):
-    os.mkdir(BUILD_PATH_RELEASE)
-
-os.chdir(BUILD_PATH_RELEASE)
-# -----------------------------------------------------------------------------
-# copy our fancy multi-thread build tool into our build_paths
-# -----------------------------------------------------------------------------
-shutil.copy(os.path.join(SCRIPT_PATH, 'jom.exe'),
-            os.path.join(BUILD_PATH_RELEASE, 'jom.exe'))
-
-subprocess.check_call(
-    CONFIGURE +
-    ' -opensource -confirm-license ' +
-    ' -nomake examples -no-compile-examples -no-cetest ' +
-    ' -qt-zlib -qt-libpng ' +
-    ' -opengl dynamic ' +
-    ' -mp -prefix \"' + PREFIX_PATH + '\" ' +
-    ' -openssl OPENSSL_LIBS_DEBUG="-llibeay32 -lssleay32" ' +
-    ' OPENSSL_LIBS_RELEASE="-llibeay32 -lssleay32" ' +
-    ' -I \"' + OPENSSL_INCLUDE_PATH + '\" -L \"' + OPENSSL_LIB_PATH + '\"' +
-    ' -no-icu ' +  # we just enable icu for the webkit module that we compile
-                   # in a separate step
-    # ' -icu ' +
-    ' -I \"' + ICU_INCLUDE_PATH + '\" -L \"' + ICU_LIB_PATH + '\"'
-    ' -release ', env=BUILD_ENV, shell=True)
-
-subprocess.check_call('jom', env=BUILD_ENV, shell=True)
-subprocess.check_call('jom install', env=BUILD_ENV, shell=True)
-
-# -----------------------------------------------------------------------------
 # we have to provide a relative prefix qt.conf file to allow qmake to work from
 # a different location.
 # -----------------------------------------------------------------------------
@@ -139,13 +181,7 @@ with open( os.path.join(PREFIX_PATH, 'bin', 'qt.conf'), 'w' ) as qt_conf_file:
     qt_conf_file.close()
 
 
-# -----------------------------------------------------------------------------
-# -----------------------------------------------------------------------------
-# RELEASE BUILD DONE !!!
-# -----------------------------------------------------------------------------
-# -----------------------------------------------------------------------------
-
 END_TIME = time.time()
 
 TOOK_SECONDS = END_TIME - START_TIME
-print '\n\nBuilding Qt5 took %02d:%02d\n' % (TOOK_SECONDS / 60, TOOK_SECONDS % 60)
+print '\n\nBuilding Qt5 in %s took %02d:%02d\n' % (MODE, TOOK_SECONDS / 60, TOOK_SECONDS % 60)
