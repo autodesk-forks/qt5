6b112550441ea03eda0da686ee5b65e8b7b6dc55
Andrii Anpilogov <andrii.anpilogov@autodesk.com>
2021-03-26 12:23:35 -0700

    Keep debug symbols

--- scripts/adsk_maya_build_qt_osx.sh
+++ scripts/adsk_maya_build_qt_osx.sh
@@ -19,41 +19,13 @@ export BUILDDIR=$WORKDIR/build
 # -opensource -confirm-license -verbose -prefix /Users/qt/work/install -debug-and-release -release -nomake tests -no-sql-mysql -plugin-sql-psql -plugin-sql-sqlite -sysconfdir /Library/Preferences/Qt -I/usr/local/opt/openssl/include
 export NUMBER_OF_PROCESSORS=`sysctl -n hw.ncpu`
 echo "make -j$NUMBER_OF_PROCESSORS"
-$SRCDIR/configure -opensource -confirm-license -verbose -prefix $INSTALLDIR -debug-and-release -force-debug-info -nomake tests -nomake examples -plugin-sql-sqlite -silent -no-strip -skip qtnetworkauth -skip qtpurchasing -no-warnings-are-errors -framework
+$SRCDIR/configure -opensource -confirm-license -verbose -prefix $INSTALLDIR -debug-and-release -force-debug-info -separate-debug-info -nomake tests -nomake examples -plugin-sql-sqlite -silent -no-strip -skip qtnetworkauth -skip qtpurchasing -no-warnings-are-errors -framework
 if [ $? -eq 0 ]; then
     make -j$NUMBER_OF_PROCESSORS
         if [ $? -eq 0 ]; then
                 make install
                 if [ $? -eq 0 ]; then
                         cd $INSTALLDIR
-
-                        for x in $(ls ./**/*.dylib); do
-                                if ! [ -L $x ]; then
-                                        echo Generating debug symbols for $x
-                                        dsymutil $x;
-                                        tar -czf $x.dSYM.tgz --directory $(dirname $x) $(basename $x).dSYM ;
-                                        rm -rf $x.dSYM;
-                                fi
-                        done
-
-                        for x in $(ls ./**/**/*.dylib); do
-                                if ! [ -L $x ]; then
-                                        echo Generating debug symbols for $x
-                                        dsymutil $x;
-                                        tar -czf $x.dSYM.tgz --directory $(dirname $x) $(basename $x).dSYM ;
-                                        rm -rf $x.dSYM;
-                                fi
-                        done
-
-                        # And remove the webkit webengine debug symbols because they are incredibly heavy
-                        # more than half of the artifact
-                        rm -vf lib/libQt5Web*.dSYM.tgz
-
-                        find . -name libQt?Core.$QTVERSION.dylib | xargs install_name_tool -rpath @executable_path/../Frameworks @loader_path/../MacOS
-                        if [ $? -ne 0 ]; then
-                            echo "**** Failed to set qtbase/core rpath ****"
-                            exit 1
-                        fi
 
                         tar -czf qt_$QTVERSION-include.tar.gz --directory=include/ . && \
                         tar -czf qt_$QTVERSION-cmake.tar.gz --directory=lib/cmake/ . && \
