134ee3b25ee59cf76de42f9c3d6568a7dfbc162f
Marcelina Viana <marcelina.viana@autodesk.com>
2024-04-18 14:14:40 +0800

    Update BuildQtOnLinux.sh

--- adsk-build-scripts/BuildQtOnLinux.sh
+++ adsk-build-scripts/BuildQtOnLinux.sh
@@ -39,8 +39,8 @@ echo "GCCVERSION=${GCCVERSION} GCCPLUSVERSION=${GCCPLUSVERSION}"
 #Setup environment variables
 #export PATH=/DATA/Qt6/tools/CMake/bin/:$PATH
 export PATH=/DATA/Qt6/tools/cmake-3.26.4-linux-x86_64/bin:$PATH
-#export CMAKE_TOOL=/DATA/Qt6/tools/CMake/bin/cmake
-export CMAKE_TOOL=/DATA/Qt6/tools/cmake-3.26.4-linux-x86_64/bin/cmake
+export CMAKE_TOOL=/usr/bin/cmake
+#export CMAKE_TOOL=/DATA/Qt6/tools/cmake-3.26.4-linux-x86_64/bin/cmake
 if [ ! -f "$CMAKE_TOOL" ]; then
   CMAKE_TOOL=cmake
 fi
