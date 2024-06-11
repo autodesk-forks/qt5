68424be6b6b2fcb1c21ff9b7e44e9838b81392d9
Huimin Wen <huimin.wen@autodesk.com>
2024-06-11 17:29:07 +0800

    1.Update the comment.

--- adsk-build-scripts/BuildQtOnLinux.sh
+++ adsk-build-scripts/BuildQtOnLinux.sh
@@ -34,7 +34,7 @@ echo "GCCVERSION=${GCCVERSION} GCCPLUSVERSION=${GCCPLUSVERSION}"
 
 if [ "${GCCVERSION}" \< "10.5.0" ] || [ "${GCCPLUSVERSION}" \< "10.5.0" ]; then
   echo "Error: need gcc 10.5.0 or higher version to build Qt 6.5.* on Linux."
-  echo "The best choice is to use gcc 11.1.0 or higher version to build Qt 6.5.* on Linux."
+  echo "Friendly reminder: the best choice is to use gcc 11.1.0 or higher version to build Qt 6.5.* on Linux."
   exit 1
 else
   echo "gcc version (${GCCVERSION}) and g++ version (${GCCPLUSVERSION}) is okay."
