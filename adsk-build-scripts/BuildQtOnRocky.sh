d23425d3c3858c2f55c6e7599cebe39fc2a483c3
neil <neil.liu@autodesk.com>
2026-01-09 15:51:52 +0800

    when build qt, use the zlib GEC build from artifactory with static libraries.

--- adsk-build-scripts/BuildQtOnRocky.sh
+++ adsk-build-scripts/BuildQtOnRocky.sh
@@ -104,6 +104,7 @@ openssl version
 find / -name "libssl.so*" 2>/dev/null
 
 export OPENSSL_ROOT_DIR=/usr/local/openssl-3.5.1
+export ZLIB_ROOT_DIR=/usr/local/zlib-1.3.1
 
 locale
 
@@ -229,7 +230,9 @@ if [[ "${CONFIG_TYPE_PARAM}" == "debug" ]]; then
                           -no-feature-designer \
                           ${QT_MODULE_SKIPPED} \
                           -- -G "Ninja" -DCMAKE_PREFIX_PATH=${LLVM_INSTALL_DIR} -DFEATURE_webengine_jumbo_build=off -DCMAKE_BUILD_TYPE=Debug \
-                          -DCMAKE_CXX_FLAGS_DEBUG="-g -Os" -DOPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR} --log-level=STATUS || exit 1
+                          -DQT_FEATURE_webengine_system_zlib=ON -DQT_FEATURE_webengine_system_minizip=ON \
+                          -DZLIB_ROOT=${ZLIB_ROOT_DIR} -DZLIB_ROOT_DIR=${ZLIB_ROOT_DIR} -DOPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR} \
+                          -DCMAKE_CXX_FLAGS_DEBUG="-g -Os" --log-level=STATUS || exit 1
 else
    LANG=${LANG} LC_ALL=${LC_ALL} LC_CTYPE=${LC_CTYPE} ${QT_ROOT_PATH}/configure -opensource -confirm-license -prefix ${CONFIG_PREFIX} -icu -opengl desktop -sql-psql \
                             -openssl-runtime -qt-libjpeg -system-zlib -feature-webengine-system-zlib -qt-harfbuzz -qt-freetype -qt-doubleconversion -xcb -release -force-debug-info -separate-debug-info \
@@ -237,7 +240,8 @@ else
                             -no-feature-designer \
                             ${QT_MODULE_SKIPPED} \
                             -- -G "Ninja" -DCMAKE_PREFIX_PATH=${LLVM_INSTALL_DIR} -DFEATURE_webengine_jumbo_build=off -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-                            -DOPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR} \
+                            -DQT_FEATURE_webengine_system_zlib=ON -DQT_FEATURE_webengine_system_minizip=ON \
+                            -DZLIB_ROOT=${ZLIB_ROOT_DIR} -DZLIB_ROOT_DIR=${ZLIB_ROOT_DIR} -DOPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR} \
                             --log-level=STATUS || exit 1
 fi
 
