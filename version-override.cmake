553d07dcca272b88d165b3cf94ed5d6b1f4109b6
neil <neil.liu@autodesk.com>
2026-02-05 14:50:01 +0800

    change the binary version from 6.8.3 to 6.83.1 to reflect security fixes by autodesk

--- version-override.cmake
+++ version-override.cmake
@@ -3,10 +3,10 @@
 
 # Override PROJECT_VERSION if it's 6.8.3
 if(PROJECT_VERSION STREQUAL "6.8.3")
-    set(PROJECT_VERSION "6.8.33")
+    set(PROJECT_VERSION "6.83.1")
     set(PROJECT_VERSION_MAJOR 6)
-    set(PROJECT_VERSION_MINOR 8)
-    set(PROJECT_VERSION_PATCH 33)
+    set(PROJECT_VERSION_MINOR 83)
+    set(PROJECT_VERSION_PATCH 1)
     set(PROJECT_VERSION_TWEAK "")
-    message(STATUS "Overriding ${PROJECT_NAME} version: 6.8.3 -> 6.8.33")
+    message(STATUS "Overriding ${PROJECT_NAME} version: 6.8.3 -> 6.83.1")
 endif()
